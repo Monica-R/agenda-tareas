668beb3d31c0233677102e7ed8e6b6337624e547
Monica-R <moniroka_339@hotmail.com>
2023-01-25 14:28:44 +0000

    Cambios en BBDD y conexión.

--- database/Connection.php
+++ database/Connection.php
@@ -10,11 +10,17 @@ Class Connection {
 
     private function setConnection(){
         $username = '';
-        $password= '';
+        $password = '';
         $server = '';
-        $dbname='';
+        $dbname = '';
+        $charset = 'utf8mb4';
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
 
-        $connection = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+        $connection = new PDO("mysql:host=$server;dbname=$dbname;charset=$charset", $username, $password, $options);
         
         $this->connection=$connection;
 
